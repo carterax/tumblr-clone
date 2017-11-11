76f64e89f27f15ef4616816fb2998ecdd9cabfe8
carterax <ikenna750@gmail.com>
2017-11-11 22:24:28 +0100

    modified index to show posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all.order('created_at DESC');
+	end
+
+	def new 
+	end
+
+	def create
+		#create a new instance of the Post Model
+		#Pass a post param arguement 
+		#save the post param arguement
+		#Redirect to the post in particular
+		@post = Post.new(post_params)
+		@post.save
+
+		redirect_to @post
+	end
+
+	def show
+		#find the post by id 
+		#and make the instance variable post available to
+		#the show view
+		@post = Post.find(params[:id])
+	end
+
+	private
+
+	 #in the post params method
+	 def post_params
+	 	params.require(:post).permit(:title, :body);
+	 end
 end
